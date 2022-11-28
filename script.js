c29d23da9e810f17ba6ea84fa1f2d4bb7b8b6320
Piotr-reader <piotr_partsianko@inbox.ru>
2022-11-28 21:02:46 +0300

    feat:implement es5 syntax

--- script.js
+++ script.js
@@ -2,41 +2,33 @@ const REQUEST_URL = "https://fe.it-academy.by/Examples/test_JSE.json";
 const SHR = new XMLHttpRequest();
 SHR.open("GET", REQUEST_URL);
 var arr;
-SHR.onload = () => {
+SHR.onload = function () {
   arr = JSON.parse(SHR.response);
-  btn();
+  btnCreate();
 };
 SHR.send();
 
 function PoemButton(buttonCaption, alertText) {
-  this.buttonCaption = buttonCaption;
-  this.alertText = alertText;
+  var self = this;
+  self.buttonCaption = buttonCaption;
+  self.alertText = alertText;
   var newEl = document.createElement("input");
   newEl.classList.add("btn");
-  newEl.value = this.buttonCaption;
-  newEl.dataset.text = this.alertText;
+  newEl.value = self.buttonCaption;
+  newEl.onclick = function () {
+    self.buttonPressed();
+  };
   document.body.appendChild(newEl);
 
-  // for (var i = 0; i < BTN.length; i++) {
-  //   BTN[i].onclick = function (e) {
-  //     var str = e.target.dataset;
-  //     console.log(str);
-  //   };
-  // }
+  self.buttonPressed = function () {
+    alert(self.alertText);
+  };
 }
 
-PoemButton.prototype.buttonPressed = function () {
-  const BTN = document.querySelector(".btn");
-  // console.log(this.alertText);
-  // return alert(text);
-};
-
-function btn() {
+function btnCreate() {
   for (var i = 0; i < arr.length; i++) {
     const element = arr[i].buttonCaption;
     const text = arr[i].alertText;
     var obj = new PoemButton(element, text);
   }
 }
-
-PoemButton.prototype.buttonPressed();
